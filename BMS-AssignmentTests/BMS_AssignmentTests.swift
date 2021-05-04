f3b77a0d94ee753511c75f2fb68defa0839776c5
Hitender Kumar <hitender.hk@gmail.com>
2021-05-04 07:00:35 +0530

    Initial commit

---
+++ BMS-AssignmentTests/BMS_AssignmentTests.swift
@@ -0,0 +1,33 @@
+//
+//  BMS_AssignmentTests.swift
+//  BMS-AssignmentTests
+//
+//  Created by Hitender Kumar on 04/05/21.
+//
+
+import XCTest
+@testable import BMS_Assignment
+
+class BMS_AssignmentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
